bb48b70de436be4c5ce7e234c9df1ae3265374d6
MatheusPitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-05-12 10:38:10 -0300

    Exercicio

--- Aula 04 - Java e Algoritmos de Ordenação e Busca/aula/exercicios.java
+++ Aula 04 - Java e Algoritmos de Ordenação e Busca/aula/exercicios.java
@@ -1,28 +1,65 @@
 import java.util.Scanner;
 
 public class exercicios {
-    public static void dados() {
-        Scanner dados = new Scanner(System.in);
 
-        System.out.println("Digite o nome do colaborador: ");
-        String nome = dados.nextLine();
-
-        System.out.println("Digite o cargo do colaborador: ");
-        String cargo = dados.nextLine();
-        
-        System.out.println("Digite a quantidade de horas extras: ");
-        int horasExtras = dados.nextInt();
-
-        System.out.println("Digite a quantidade de faltas: ");
-        int faltas = dados.nextInt();
-
-        System.out.println("O colaborador usa Plano de saude? ");
-        String planoDeSaude = dados.next();
-
-    }
     public static void main(String[] args) {
         
-    }
-    }
+        public Void cadastro(); {
+            Scanner dados = new Scanner(System.in);
+    
+            System.out.println("Digite o nome do colaborador: ");
+            String nome = dados.nextLine();
+    
+            System.out.println("Digite o cargo do colaborador: ");
+            String cargo = dados.nextLine();
+    
+            System.out.println("Digite a quantidade de horas extras: ");
+            int horasExtras = dados.nextInt();
+    
+            System.out.println("Digite a quantidade de faltas: ");
+            int faltas = dados.nextInt();
+    
+            System.out.println("O colaborador usa Plano de saude? ");
+            String planoDeSaude = dados.next();
+        } 
+       
+        public static void cadastro() {
+            Scanner dados = new Scanner(System.in);
+    
+            System.out.println("Digite o nome do colaborador: ");
+            String nome = dados.nextLine();
+    
+            System.out.println("Digite o cargo do colaborador: ");
+            String cargo = dados.nextLine();
+    
+            System.out.println("Digite a quantidade de horas extras: ");
+            int horasExtras = dados.nextInt();
+    
+            System.out.println("Digite a quantidade de faltas: ");
+            int faltas = dados.nextInt();
+    
+            System.out.println("O colaborador usa Plano de saude? ");
+            String planoDeSaude = dados.next();
+        }
+        
+        while (true) {
 
+            if (opcao = 1) {
+                cadastro();
+            }
+
+            if (opcao = 2) {
+                mostraCadastro();
+            }
+
+            if (opcao = 3) {
+                alteraCadastro();
+            }
+
+            if (opcao = 0) {
+                break;
+            }
+
+        }
+    }
 }
