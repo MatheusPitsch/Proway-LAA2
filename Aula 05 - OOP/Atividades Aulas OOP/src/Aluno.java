a764a3a365ad07f49d3923cf01d97a1aaa5d9767
MatheusPitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-05-17 21:57:14 -0300

    Exercicios Aula 05

--- Aula 05 - OOP/Atividades Aulas OOP/src/Aluno.java
+++ Aula 05 - OOP/Atividades Aulas OOP/src/Aluno.java
@@ -1,80 +1,72 @@
+
 public class Aluno {
+    
     private int matricula;
     private String nome;
-    private double n1;
-    private double n2;
-    private double trabalho;
-    public char[] getN1;
+    private float n1;
+    private float n2;
+    private float trabalho;
 
-    public Aluno(int matricula, String nome){
+    
+
+    public Aluno(int matricula, String nome) {
         this.matricula = matricula;
-        this.setNome(nome);
+        this.nome = nome;
     }
 
-    public int getMatricula(){
+    public int getMatricula() {
         return matricula;
     }
-    
-    public String getNome() {
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome(){
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome){
         this.nome = nome;
     }
-    
-    public double getN1(){
+
+    public float getN1(){
         return n1;
     }
 
-    public void setN1(double n1){
+    public void setN1(float n1){
         this.n1 = n1;
-
-        if(n1 >= 11){
-            System.out.println("Nota invalida.");
-        }
     }
 
-    public double getN2(){
+    public float getN2(){
         return n2;
     }
-    
-    public void setN2(double n2){
+
+    public void setN2(float n2){
         this.n2 = n2;
-        if(n1 >= 11){
-            System.out.println("Nota invalida.");
-        }
     }
     
-    public double getTrabalho() {
+    public float getTrabalho() {
         return trabalho;
     }
 
-    public void setTrabalho(double trabalho) {
+    public void setTrabalho(Float trabalho) {
         this.trabalho = trabalho;
-        if(n1 >= 11){
-            System.out.println("Nota invalida.");
-        }
     }
-     
-    public double getMedia(double n1,double n2, double trabalho){
-        double media = (n1*3+n2*3+trabalho*2) / 8;
+
+    public float getMedia(){
+        float media = (n1*3+n2*3+trabalho*2) / 8;
         return media;
     }
 
-    public void getSituacao(double media){
-        
-        if(media < 5){
-            System.out.println("Aluno Reprovado");
+    public void getSituacao(float media){
+        if (media <= 4.99) {
+            System.out.println("Reprovado");
+        } else if(media <= 6.99 ){
+            System.out.println("Recuperação");
+        }else if(media <= 10){
+            System.out.println("Aprovado");
         }
-        if (media < 7) {
-            System.out.println( "Aluno em recuperação");
-        }
-        if (media <= 10) {
-            System.out.println("Aluno Aprovado");
-        }
-        
-
     }
-
 }
+
--- Aula 05 - OOP/Atividades Aulas OOP/src/TesteAluno.java
+++ Aula 05 - OOP/Atividades Aulas OOP/src/TesteAluno.java
@@ -1,19 +1,19 @@
-import javax.print.attribute.standard.Media;
-
 public class TesteAluno {
     
-
     public static void main(String[] args) {
         
-        
-        Aluno aluno = new Aluno(111,"Matheus");
-        
-        aluno.setN1(8);
-        aluno.setN2(9);
-        aluno.setTrabalho(8.5);
+        Aluno al1 = new Aluno(111, "Matheus");
 
-        System.out.println(aluno.getMedia(6, 9, 8.5));
+        System.out.println(al1.getMatricula());
+        System.out.println(al1.getNome());
+
+        al1.setN1(5);
+        al1.setN2(5);
+        al1.setTrabalho((float) 5.5);
+
+        System.out.println(al1.getMedia());
+        al1.getSituacao(al1.getMedia());
+        
+
     }
-    
-
 }
