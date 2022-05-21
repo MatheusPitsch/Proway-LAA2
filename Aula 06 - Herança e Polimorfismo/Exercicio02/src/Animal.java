4a347bd9d55330c908c20522860be2dd0fc65f91
MatheusPitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-05-21 18:30:08 -0300

    Aula 06

---
+++ Aula 06 - Herança e Polimorfismo/Exercicio02/src/Animal.java
@@ -0,0 +1,48 @@
+package Exercicio02.src;
+
+public class Animal {
+    private String nome;
+    private double comprimento;
+    private int numPatas;
+    private String cor;
+    private String ambiante;
+    private double velMedia;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getComprimento() {
+        return comprimento;
+    }
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+    public int getNumPatas() {
+        return numPatas;
+    }
+    public void setNumPatas(int numPatas) {
+        this.numPatas = numPatas;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getAmbiante() {
+        return ambiante;
+    }
+    public void setAmbiante(String ambiante) {
+        this.ambiante = ambiante;
+    }
+    public double getVelMedia() {
+        return velMedia;
+    }
+    public void setVelMedia(double velMedia) {
+        this.velMedia = velMedia;
+    }
+    
+}
